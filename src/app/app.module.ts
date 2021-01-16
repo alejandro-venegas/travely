5cf301f463901a5facf93f2931b3ab94575e8214
Alejandro Venegas <avenegasc14@gmail.com>
2021-01-15 22:20:09 -0600

    Refactor header and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HeaderComponent } from './header/header.component'
-import { FooterComponent } from './footer/footer.component'
-import { MenuDirective } from './header/menu.directive'
+import { HeaderComponent } from './components/header/header.component'
+import { FooterComponent } from './components/footer/footer.component'
+import { MenuDirective } from './components/header/menu.directive'
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, MenuDirective],
