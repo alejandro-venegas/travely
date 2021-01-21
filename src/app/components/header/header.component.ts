013fee72546381a82dd8973c39efcf0b094b52c1
Alejandro Venegas <avenegasc14@gmail.com>
2021-01-20 23:06:38 -0600

    Minor header front end improvement

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   isMenuActive = false;
+  @Output() menuActivated = new EventEmitter<boolean>();
   constructor() {}
 
   ngOnInit(): void {}
 
   toggleMenu(): void {
     this.isMenuActive = !this.isMenuActive;
+    this.menuActivated.emit(this.isMenuActive);
   }
 }
