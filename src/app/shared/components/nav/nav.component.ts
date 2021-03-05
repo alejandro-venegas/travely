41f3c72422dc494a0f7c12166440a9e852b00692
Alejandro Venegas <avenegasc14@gmail.com>
2021-03-05 16:30:28 -0600

    Hide menu when click on item

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuService } from '../../services/menu.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  constructor(private router: Router, private menuService: MenuService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onNavClick(route: string): void {
+    this.menuService.isMenuActiveSubject.next(false);
+    this.router.navigate([route]);
   }
-
 }
