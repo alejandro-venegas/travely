a75e140900dfc54554c12189e1083f838524e9d3
Alejandro Venegas <avenegasc14@gmail.com>
2021-03-29 23:18:05 -0600

    Add geonames service

--- src/app/shared/services/geonames.service.ts
+++ src/app/shared/services/geonames.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country.interface';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { City } from '../interfaces/city.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +12,25 @@ import { environment } from '../../../environments/environment';
 export class GeonamesService {
   private _baseUrl = 'http://api.geonames.org/';
   private _username = environment.geoNamesUsername;
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  fetchCountries(): Observable<Country[]> {
+    const url = this._baseUrl + 'countryInfoJSON?username=' + this._username;
+    return this.http
+      .get<{ geonames: Country[] }>(url)
+      .pipe(map((value) => value.geonames));
+  }
+
+  fetchCities(countryCode: string): Observable<City[]> {
+    const url =
+      this._baseUrl +
+      'searchJSON?username=' +
+      this._username +
+      '&country=' +
+      countryCode;
+
+    return this.http
+      .get<{ totalResultsCount: number; geonames: City[] }>(url)
+      .pipe(map((value) => value.geonames));
+  }
 }
