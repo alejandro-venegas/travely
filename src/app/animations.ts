cbbe082a06c9466e420069cb7828714b48de408e
Alejandro Venegas <avenegasc14@gmail.com>
2021-05-20 08:53:44 -0600

    Refactor route animation

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -16,6 +16,7 @@ export const fader = trigger('routeAnimations', [
       [
         style({
           position: 'absolute',
+          width: '100%',
           top: 0,
           left: 0,
           opacity: 1,
